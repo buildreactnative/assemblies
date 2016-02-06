8c717a1ba81148a940d04a3f6c5a0b5b1d0ae630
tomatagolo <tom@agolo.com>
2016-02-06 16:28:48 -0500

    add top panel to Group page

--- application/groups/group.js
+++ application/groups/group.js
@@ -39,7 +39,14 @@ class Group extends React.Component{
         tintColor={Colors.brandPrimary}
         leftButton={backButton}
       />
-        <Text>GROUP</Text>
+        <Image source={{uri: group.backgroundImage}} style={styles.topImage}>
+          <View style={styles.overlayBlur}>
+            <Text style={styles.h1}>{group.name}</Text>
+          </View>
+          <View style={styles.bottomPanel}>
+            <Text style={styles.memberText}>{group.memberCount} member</Text>
+          </View>
+        </Image>
       </View>
     )
   }
@@ -54,7 +61,39 @@ let styles = {
   container: {
     flex: 1,
   },
-  
+  topImage: {
+    width: deviceWidth,
+    height: 200,
+    flexDirection: 'column',
+  },
+  overlayBlur: {
+    backgroundColor: '#333',
+    opacity: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  h1: {
+    fontSize: 22,
+    color: 'white',
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  bottomPanel: {
+    flex: 0.3,
+    backgroundColor: 'white',
+    opacity: 0.8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  memberText: {
+    textAlign: 'center',
+    color: Colors.brandPrimary,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+
+
 
 }
 
