fab955cc6fd79d062d14df286990785c74d004d6
tomatagolo <tom@agolo.com>
2016-03-20 14:39:59 -0400

    allow to join / unsubscribe from calendar view

--- application/calendar/calendar_view.js
+++ application/calendar/calendar_view.js
@@ -33,6 +33,50 @@ const CUSTOM_CONFIG = Navigator.SceneConfigs.HorizontalSwipeJump;
 CUSTOM_CONFIG.gestures = {}; // disable gestures for side swipe
 
 export default class CalendarView extends Component{
+  unsubscribe(group, currentUser){
+    let idx = _.findIndex(this.props.suggestedGroups, (g) => g.id == group.id);
+    if (idx != -1){
+      this.props.sendData({
+        groups          : _.reject(this.props.groups, (g) => g.id == group.id),
+        suggestedGroups : [...this.props.suggestedGroups.slice(0, idx), group, ...this.props.suggestedGroups.slice(idx+1)],
+      });
+    } else {
+      this.props.sendData({
+        groups          : _.reject(this.props.groups, (g) => g.id == group.id),
+        suggestedGroups : this.props.suggestedGroups.concat(group),
+      });
+    }
+    this.props.updateUser(currentUser);
+    let url = `${BASE_URL}/groups/${group.id}`;
+    fetch(url, {
+      method    : 'PUT',
+      headers   : HEADERS,
+      body      : JSON.stringify({members: group.members})
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      let url = `${BASE_URL}/users/${currentUser.id}`;
+      fetch(url, {
+        method    : 'PUT',
+        headers   : HEADERS,
+        body      : JSON.stringify({groupIds: currentUser.groupIds})
+      })
+      .catch((err) => {
+        if (DEV) {console.log('ERR: ', err);}
+      }).done();
+    })
+    .catch((err) => {
+      if (DEV) {console.log('ERR:', err);}
+    }).done();
+  }
+  addUserToGroup(group, currentUser){
+    let {suggestedGroups} = this.props;
+    let idx = _.findIndex(suggestedGroups, (g) => g.id == group.id);
+    this.props.sendData({
+      suggestedGroups: [...suggestedGroups.slice(0, idx), group, ...suggestedGroups.slice(idx+1)],
+    });
+    this.props.updateUser(currentUser);
+  }
   render(){
     return (
       <View style={styles.container}>
@@ -59,31 +103,16 @@ export default class CalendarView extends Component{
               return (
                 <Profile {...route} {...this.props} navigator={navigator} />
               )
-            } else if (route.name == 'Groups') {
-              return (
-                <Groups
-                  {...this.props}
-                  navigator={navigator}
-                  addUserToGroup={()=>{
-                    if (DEV) {console.log('ADD USER TO GROUP')}
-                  }}
-                />
-              )
-            } else if (route.name == 'CreateGroup'){
-              return <CreateGroup {...this.props} navigator={navigator} />
             } else if (route.name == 'Group') {
               return (
                 <Group
-                  addUserToGroup={()=>{
-                    if (DEV) {console.log('ADD USER TO GROUP')}
-                  }}
+                  addUserToGroup={this.addUserToGroup.bind(this)}
+                  unsubscribe={this.unsubscribe.bind(this)}
                   {...this.props}
                   {...route}
                   navigator={navigator}
                 />
               )
-            } else if (route.name == 'Members') {
-              return <GroupMembers {...this.props} navigator={navigator} />
             } else if (route.name == 'Events' ) {
               return <GroupEvents {...this.props} navigator={navigator}  />
             } else if (route.name == 'CreateEvent'){
@@ -95,15 +124,6 @@ export default class CalendarView extends Component{
                   addEvent={()=>{
                     if (DEV) {console.log('ADD EVENT')}
                   }}
-                />
-              )
-            } else if (route.name == 'CreateGroupConfirm'){
-              return (
-                <CreateGroupConfirm {...this.props} {...route}
-                  createGroup={()=> {
-                    if (DEV) {console.log('CREATE GROUP')}
-                  }}
-                  navigator={navigator}
                 />
               )
             } else if (route.name == 'Chat') {
