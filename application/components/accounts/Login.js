3fe01863bc807f0e548c7ebf67f48e11e90a7503
tomatagolo <tom@agolo.com>
2016-07-25 09:40:27 -0400

    Add basic Login component

--- application/components/accounts/Login.js
+++ application/components/accounts/Login.js
@@ -1,33 +1,88 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { globals } from '../../styles';
 import NavigationBar from 'react-native-navbar';
-import Icon from 'react-native-vector-icons/Ionicons';
-import BackButton from '../shared/BackButton';
+import React, { Component } from 'react';
+import { Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { extend } from 'underscore';
+
 import Colors from '../../styles/colors';
+import BackButton from '../shared/BackButton';
+import { globals, formStyles } from '../../styles';
+
+const styles = formStyles;
 
 class Login extends Component{
   constructor(){
     super();
+    this.loginUser = this.loginUser.bind(this);
     this.goBack = this.goBack.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+    this.changeEmail = this.changeEmail.bind(this);
+    this.state = {
+      email           : '',
+      password        : '',
+      errorMsg        : '',
+    };
+  }
+  loginUser(){
+    /* TODO: login user with username and password */
   }
   goBack(){
     this.props.navigator.pop();
+  }
+  changeEmail(email){
+    this.setState({ email })
+  }
+  changePassword(password){
+    this.setState({ password })
   }
   render(){
     return (
       <View style={globals.flexContainer}>
         <NavigationBar
+          leftButton={<BackButton handlePress={this.goBack} />}
           title={{ title: 'Login', tintColor: 'white' }}
           tintColor={Colors.brandPrimary}
-          leftButton={<BackButton handlePress={this.goBack}/>}
         />
-        <View style={globals.flexCenter}>
-          <Text style={globals.h2}>Login</Text>
-        </View>
+        <ScrollView style={styles.container}>
+          <Text style={styles.h3}>Login with your email and password.</Text>
+          <Text style={styles.h4}>Email</Text>
+          <View style={styles.formField}>
+            <TextInput
+              autoFocus={true}
+              returnKeyType="next"
+              onSubmitEditing={() => this.password.focus()}
+              onChangeText={this.changeEmail}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              maxLength={140}
+              placeholderTextColor={Colors.copyMedium}
+              style={styles.input}
+              placeholder="Your email address"
+            />
+          </View>
+          <Text style={styles.h4}>Password</Text>
+          <View style={styles.formField}>
+            <TextInput
+              ref={(el) => this.password = el }
+              returnKeyType="next"
+              onChangeText={this.changePassword}
+              secureTextEntry={true}
+              autoCapitalize="none"
+              maxLength={140}
+              placeholderTextColor={Colors.copyMedium}
+              style={styles.input}
+              placeholder="Your password"
+            />
+          </View>
+          <View style={styles.errorContainer}>
+            <Text style={styles.errorText}>{this.state.errorMsg}</Text>
+          </View>
+        </ScrollView>
+        <TouchableOpacity style={styles.submitButton} onPress={this.loginUser}>
+          <Text style={globals.largeButtonText}>Login</Text>
+        </TouchableOpacity>
       </View>
     )
   }
-};
+}
 
 export default Login;
